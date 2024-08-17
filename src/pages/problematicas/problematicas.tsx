a74cd15cbca0c9444394b0eadf82258ffe9ec984
IVANCCAMA <ccamaivan1@gmail.com>
2024-08-17 06:33:35 -0400

    refactor: Update problematicas page table columns and data source

--- src/pages/problematicas/problematicas.tsx
+++ src/pages/problematicas/problematicas.tsx
@@ -1,12 +1,10 @@
+/* NO TOCAR esto, porque se rompe xd */
 import { createTheme } from "@mui/material/styles";
 import { ChangeEvent, MouseEvent, useMemo, useState } from "react";
 import Box from "@mui/material/Box";
 import Table from "@mui/material/Table";
-import TableBody from "@mui/material/TableBody";
-import TableCell from "@mui/material/TableCell";
 import TableContainer from "@mui/material/TableContainer";
 import TablePagination from "@mui/material/TablePagination";
-import TableRow from "@mui/material/TableRow";
 import Typography from "@mui/material/Typography";
 import Paper from "@mui/material/Paper";
 import FormControlLabel from "@mui/material/FormControlLabel";
