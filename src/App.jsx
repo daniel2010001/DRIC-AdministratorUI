1bb2ecb87e68c5f2772f411678ca3a97bb888dba
IVANCCAMA <ccamaivan1@gmail.com>
2024-08-18 23:15:26 -0400

    refactor: Remove unused imports and update routes in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import { Background, Login, Problematicas, ProblemForm } from "./pages";
 import { Route, Routes } from "react-router-dom";
-import { Problematicas } from "./pages/problematicas/problematicas";
 
 function App() {
   return (
     <Background>
       <Routes>
         <Route path="/" element={<Problematicas />} />
-        <Route path="/problems" element={<Problematicas />} />
         <Route path="/forms" element={<ProblemForm />} />
         <Route path="/login" element={<Login />} />
       </Routes>
